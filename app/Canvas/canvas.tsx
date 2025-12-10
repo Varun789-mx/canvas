d08f1d30204687b511aa843ae4e980a93b0517f5
varun789-mx <varunsh153@gmail.com>
2025-12-11 02:00:36 +0530

    Add canvas line circle and class to controll all the tools

--- app/Canvas/canvas.tsx
+++ app/Canvas/canvas.tsx
@@ -1,46 +1,68 @@
-import { Caveat } from "next/font/google";
 import React, { useEffect, useRef, useState } from "react"
+import { CanvasDrawer } from "../lib/CanvasClass";
 
 const Canvas = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const [start, setstart] = useState({ x: 0, y: 0 });
     const [drawing, setdrawing] = useState(false);
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        if (!canvas) return;
-
-        const ctx = canvas.getContext('2d');
-        if (!ctx) return;
-
-        ctx.fillStyle = 'white';
-        ctx.strokeRect(10, 10, 100, 100);
-    }, [])
 
     function HandleMouseDown(e: React.MouseEvent<HTMLCanvasElement>) {
         setdrawing(true);
         setstart({ x: e.clientX, y: e.clientY });
     }
-    function CreateRectangle(e: React.MouseEvent<HTMLCanvasElement>) {
-        const canvas = canvasRef.current;
-        if (!canvas || !drawing) return;
+    // function CreateRectangle(e: React.MouseEvent<HTMLCanvasElement>) {
+    //     const canvas = canvasRef.current;
+    //     if (!canvas || !drawing) return;
 
-        const height = e.clientX - start.x;
-        const width = e.clientY - start.y;
+    //     const height = e.clientX - start.x;
+    //     const width = e.clientY - start.y;
 
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        const ctx = canvas.getContext('2d');
-        if (!ctx) return;
-        ctx.reset();
-        ctx.beginPath();
-        ctx.moveTo(start.x, start.y);
-        ctx.lineTo(e.clientX, e.clientY);
-        
-    }
+    //     canvas.width = window.innerWidth;
+    //     canvas.height = window.innerHeight;
+    //     const ctx = canvas.getContext('2d');
+    //     if (!ctx) return;
+    //     ctx.strokeStyle = 'white'
+    //     ctx.roundRect(start.x, start.y, height, width, 5 * Math.PI);
+    //     ctx.stroke();
+    // }
+    // function CreateCircle(e: React.MouseEvent<HTMLCanvasElement>) {
+    //     const canvas = canvasRef.current;
+    //     if (!canvas || !drawing) return;
 
+    //     const height = e.clientY - start.y
+    //     canvas.width = window.innerWidth;
+    //     canvas.height = window.innerHeight;
+    //     const ctx = canvas.getContext('2d');
+    //     if (!ctx) return;
+    //     ctx.strokeStyle = 'white'
+    //     ctx.arc(start.x, start.y, Math.abs(height), 0, 2 * Math.PI);
+    //     ctx.stroke();
+    // }
+    // function CreateLine(e: React.MouseEvent<HTMLCanvasElement>) {
+    //     const canvas = canvasRef.current;
+    //     if (!canvas || !drawing) return;
+
+    //     const height = e.clientY - start.y
+    //     const width = e.clientX - start.x;
+
+    //     canvas.width = window.innerWidth;
+    //     canvas.height = window.innerHeight;
+
+    //     const ctx = canvas.getContext('2d');
+    //     if (!ctx) return;
+    //     ctx.strokeStyle = 'white'
+    //     ctx.beginPath();
+    //     ctx.moveTo(start.x, start.y);
+    //     ctx.lineTo(e.clientX, e.clientY);
+    //     ctx.stroke();
+    // }
     function HandleMouseUp() {
         setdrawing(false);
     }
+
+    const canvas = canvasRef.current
+    if (!canvas) return;
+    const drawer = new CanvasDrawer(canvas);
     return (
         <canvas ref={canvasRef}
             id="canvas"
