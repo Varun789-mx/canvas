9b0cb9372782a7d3b8c72798cd9694bf9f7b6734
Harsh <VARUNSH153@GMAIL.COM>
2025-12-11 16:29:53 +0530

    Connect class to the function

--- app/Canvas/canvas.tsx
+++ app/Canvas/canvas.tsx
@@ -1,75 +1,50 @@
-import React, { useEffect, useRef, useState } from "react"
+import React, { useRef, useState } from "react";
 import { CanvasDrawer } from "../lib/CanvasClass";
 
-const Canvas = () => {
-    const canvasRef = useRef<HTMLCanvasElement>(null);
-    const [start, setstart] = useState({ x: 0, y: 0 });
-    const [drawing, setdrawing] = useState(false);
-
-    function HandleMouseDown(e: React.MouseEvent<HTMLCanvasElement>) {
-        setdrawing(true);
-        setstart({ x: e.clientX, y: e.clientY });
-    }
-    // function CreateRectangle(e: React.MouseEvent<HTMLCanvasElement>) {
-    //     const canvas = canvasRef.current;
-    //     if (!canvas || !drawing) return;
-
-    //     const height = e.clientX - start.x;
-    //     const width = e.clientY - start.y;
-
-    //     canvas.width = window.innerWidth;
-    //     canvas.height = window.innerHeight;
-    //     const ctx = canvas.getContext('2d');
-    //     if (!ctx) return;
-    //     ctx.strokeStyle = 'white'
-    //     ctx.roundRect(start.x, start.y, height, width, 5 * Math.PI);
-    //     ctx.stroke();
-    // }
-    // function CreateCircle(e: React.MouseEvent<HTMLCanvasElement>) {
-    //     const canvas = canvasRef.current;
-    //     if (!canvas || !drawing) return;
-
-    //     const height = e.clientY - start.y
-    //     canvas.width = window.innerWidth;
-    //     canvas.height = window.innerHeight;
-    //     const ctx = canvas.getContext('2d');
-    //     if (!ctx) return;
-    //     ctx.strokeStyle = 'white'
-    //     ctx.arc(start.x, start.y, Math.abs(height), 0, 2 * Math.PI);
-    //     ctx.stroke();
-    // }
-    // function CreateLine(e: React.MouseEvent<HTMLCanvasElement>) {
-    //     const canvas = canvasRef.current;
-    //     if (!canvas || !drawing) return;
-
-    //     const height = e.clientY - start.y
-    //     const width = e.clientX - start.x;
-
-    //     canvas.width = window.innerWidth;
-    //     canvas.height = window.innerHeight;
-
-    //     const ctx = canvas.getContext('2d');
-    //     if (!ctx) return;
-    //     ctx.strokeStyle = 'white'
-    //     ctx.beginPath();
-    //     ctx.moveTo(start.x, start.y);
-    //     ctx.lineTo(e.clientX, e.clientY);
-    //     ctx.stroke();
-    // }
-    function HandleMouseUp() {
-        setdrawing(false);
-    }
-
-    const canvas = canvasRef.current
-    if (!canvas) return;
-    const drawer = new CanvasDrawer(canvas);
-    return (
-        <canvas ref={canvasRef}
-            id="canvas"
-            onMouseDown={HandleMouseDown}
-            onMouseUp={HandleMouseUp}
-            onMouseMove={CreateRectangle}
-            className="bg-gray-800 w-screen h-screen" />
-    )
+enum ShapeProp {
+  circle = "Circle",
+  rectangele = "Rectangle",
+  line = "Line",
+  ellipse = "Ellipse",
 }
-export default Canvas
+const Canvas = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [start, setstart] = useState({ x: 0, y: 0 });
+  const [shape, setshape] = useState<ShapeProp>(ShapeProp.line);
+  const [drawing, setdrawing] = useState(false);
+
+  function HandleMouseDown(e: React.MouseEvent<HTMLCanvasElement>) {
+    setdrawing(true);
+    setstart({ x: e.clientX, y: e.clientY });
+  }
+
+  function HandleMouseUp() {
+    setdrawing(false);
+  }
+  const HandleDrawing = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    if (drawing) {
+      const drawer = new CanvasDrawer(canvas);
+      drawer.CreateRectangle(start.x, start.y, e.clientX, e.clientY);
+    }
+  };
+  return (
+    <div>
+      <div className="w-3/4 flex items bg-white ">
+        <div>Circle</div>
+        <div>Rectangle</div>
+        <div>line</div>
+      </div>
+      <canvas
+        ref={canvasRef}
+        id="canvas"
+        onMouseDown={HandleMouseDown}
+        onMouseUp={HandleMouseUp}
+        onMouseMove={HandleDrawing}
+        className="bg-gray-800 w-screen h-screen"
+      />
+    </div>
+  );
+};
+export default Canvas;
--- app/lib/CanvasClass.ts
+++ app/lib/CanvasClass.ts
@@ -1,49 +1,52 @@
 export class CanvasDrawer {
-    private canvas: HTMLCanvasElement;
-    private ctx: CanvasRenderingContext2D;
+  private canvas: HTMLCanvasElement;
+  private ctx: CanvasRenderingContext2D;
 
+  constructor(canvas: HTMLCanvasElement) {
+    this.canvas = canvas;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) throw new Error("Unable to get the canas element");
+    this.ctx = ctx;
+    this.init();
+  }
 
-    constructor(canvas: HTMLCanvasElement) {
-        this.canvas = canvas;
-        const ctx = canvas.getContext('2d');
-        if (!ctx) throw new Error("Unable to get the canas element");
-        this.ctx = ctx;
-        this.init();
+  private init() {
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+    this.ctx.strokeStyle = "white";
+    this.ctx.lineWidth = 1;
+  }
+
+  clear() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+  CreateRectangle(
+    startX: number,
+    startY: number,
+    endX: number,
+    endY: number,
+    rounded?: boolean
+  ) {
+    const height = endY - startY;
+    const width = endX - startX;
+
+    this.ctx.strokeStyle = "white";
+    if (rounded) {
+      this.ctx.roundRect(startX, startY, width, height, 5 * Math.PI);
+      this.ctx.stroke();
     }
+    this.ctx.strokeRect(startX, startY, width, height);
+  }
 
-    private init() {
-        this.canvas.width = window.innerWidth;
-        this.canvas.height = window.innerHeight;
-        this.ctx.strokeStyle = 'white';
-        this.ctx.lineWidth = 1;
-    }
-
-    clear() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-    CreateRectangle(startX: number, startY: number, endX: number, endY: number, rounded: boolean) {
-
-        const height = endY - startY;
-        const width = endX - startX;
-
-        this.ctx.strokeStyle = 'white'
-        if (!rounded) {
-            this.ctx.strokeRect(startX, startY, height, width)
-        }
-        this.ctx.roundRect(startX, startY, height, width, 5 * Math.PI);
-        this.ctx.stroke();
-    }
-    CreateCircle(startX: number, startY: number, endX: number, endY: number) {
-        const height = endY - startY;
-        this.ctx.arc(startX, startY, Math.abs(height), 0, 2 * Math.PI);
-        this.ctx.stroke();
-    }
-    CreateLine(startX: number, startY: number, endX: number, endY: number) {
-
-        this.ctx.beginPath();
-        this.ctx.moveTo(startX, startY);
-        this.ctx.lineTo(endX, endY);
-        this.ctx.stroke();
-    }
-
+  CreateCircle(startX: number, startY: number, endX: number, endY: number) {
+    const height = endY - startY;
+    this.ctx.arc(startX, startY, Math.abs(height), 0, 2 * Math.PI);
+    this.ctx.stroke();
+  }
+  CreateLine(startX: number, startY: number, endX: number, endY: number) {
+    this.ctx.beginPath();
+    this.ctx.moveTo(startX, startY);
+    this.ctx.lineTo(endX, endY);
+    this.ctx.stroke();
+  }
 }
